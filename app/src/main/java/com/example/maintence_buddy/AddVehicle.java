7b2e3403e2d52b1af88d207bcec146288467b923
Owen Thronson <othronson@wisc.edu>
2021-11-01 12:03:55 -0500

    Add Vehicle fragment

---
+++ app/src/main/java/com/example/maintence_buddy/AddVehicle.java
@@ -0,0 +1,36 @@
+package com.example.maintence_buddy;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AddVehicle extends Fragment {
+
+    private AddVehicleViewModel mViewModel;
+
+    public static AddVehicle newInstance() {
+        return new AddVehicle();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.add_vehicle_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(AddVehicleViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/maintence_buddy/AddVehicleViewModel.java
@@ -0,0 +1,7 @@
+package com.example.maintence_buddy;
+
+import androidx.lifecycle.ViewModel;
+
+public class AddVehicleViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
